d4cfa0bc3f75d6194e64d6e91f7eb51da1e1c0f8
Ganjiguur Natsagdorj <ganjiguur.n@gmail.com>
2026-06-13 14:32:03 +0100

    error

--- triagnostics/src/pages/Bonus.tsx
+++ triagnostics/src/pages/Bonus.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useQuizStore } from "../store/quizStore";
 import { bonusQuestions } from "../data/index";
